232813fcd74d89a8cfce63995b1d7d2e4a543689
Gaurav Nolkha <gnolkha@gmail.com>
2017-08-28 17:58:17 -0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,16 +1,17 @@
 module.exports = {
   port: 3000,
   aws: {
-    accessId: process.env.ACCESS_KEY_ID || "AKIAJETAJ62TNCEFZCMA",
-    secret: process.env.SECRET_ACCESS_KEY || "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",
+    accessId: process.env.ACCESS_KEY_ID,
+    secret: process.env.SECRET_ACCESS_KEY,
     bucket: 'coding-challenges',
     base: "https://coding-challenges.s3.amazonaws.com"
   },
   routes: {
     files: '/api/files',
-    projects: '/api/projects'
+    projects: '/api/projects',
+    folders: '/api/folders'
   },
   db: {
-    url: 'mongodb://heroku_vnj7nqfj:lfqk9oj7uakj5cdsdml5m7je2t@ds143707.mlab.com:43707/heroku_vnj7nqfj'
+    url: process.env.MONGODB_URL
   }
 }
